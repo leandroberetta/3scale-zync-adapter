f3fbb796d1d7ec9abc51903fa415058572a4d3d2
Leandro Beretta <lea.beretta@gmail.com>
2020-11-19 08:09:33 -0300

    logger format

--- app.py
+++ app.py
@@ -3,45 +3,46 @@ import logging
 import requests
 import os
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
 
 app = Flask(__name__)
 
 
 def get_auth_token():
     data = {
-        "grant_type": "password",
-        "username": os.environ.get("IDP_USERNAME"),
-        "password": os.environ.get("IDP_PASSWORD"),
-        "client_id": "admin-cli"
+        'grant_type': 'password',
+        'username': os.environ.get('IDP_USERNAME'),
+        'password': os.environ.get('IDP_PASSWORD'),
+        'client_id': 'admin-cli'
     }
 
-    response = requests.post("{}/protocol/openid-connect/token".format(
-        os.environ.get("IDP_MASTER_REALM_URL")), verify=tls_verify(), data=data)
+    response = requests.post('{}/protocol/openid-connect/token'.format(
+        os.environ.get('IDP_MASTER_REALM_URL')), verify=tls_verify(), data=data)
 
     app.logger.info(response.json())
 
-    return response.json()["access_token"]
+    return response.json()['access_token']
 
 
 def tls_verify():
-    if os.environ.get("IDP_VERIFY_TLS") == "false":
+    if os.environ.get('IDP_VERIFY_TLS') == 'false':
         return False
 
     return True
 
 
 def get_client(clientId):
-    response = requests.get("{}/clients".format(os.environ.get("IDP_ADMIN_REALM_URL")),
-                            verify=tls_verify(), 
-                            headers={"Authorization": "Bearer {}".format(get_auth_token())})
+    response = requests.get('{}/clients'.format(os.environ.get('IDP_ADMIN_REALM_URL')),
+                            verify=tls_verify(),
+                            headers={'Authorization': 'Bearer {}'.format(get_auth_token())})
 
     clients = response.json()
-    app.logger.info(clients)
+    
     for client in clients:
         app.logger.info(client)
-        if client["clientId"] == clientId:
-            app.logger.info(client["id"])
+        if client['clientId'] == clientId:
+            app.logger.info(client['id'])
             return client
 
     return None
@@ -49,8 +50,8 @@ def get_client(clientId):
 
 @app.route('/.well-known/openid-configuration', methods=['GET'])
 def get_openid_configuration():
-    response = requests.get("{}/.well-known/openid-configuration".format(
-        os.environ.get("IDP_REALM_URL")), verify=tls_verify())
+    response = requests.get('{}/.well-known/openid-configuration'.format(
+        os.environ.get('IDP_REALM_URL')), verify=tls_verify())
 
     return response.json()
 
@@ -60,30 +61,26 @@ def create_or_update(clientId):
     data = request.json
 
     new_or_updated_client = {
-        "clientId": data["client_id"],
-        "secret": data["client_secret"],
-        "enabled": True,
-        "publicClient": False
+        'clientId': data['client_id'],
+        'secret': data['client_secret'],
+        'enabled': True,
+        'publicClient': False
     }
-
-    app.logger.info("new_or_updated_client")
-    app.logger.info(new_or_updated_client)
 
     client = get_client(clientId)
 
     if client is not None:
-        response = requests.put("{}/clients/{}".format(os.environ.get("IDP_ADMIN_REALM_URL"),
-                                                       client["id"]),
+        requests.put('{}/clients/{}'.format(os.environ.get('IDP_ADMIN_REALM_URL'),
+                                            client['id']),
                                 json=new_or_updated_client,
                                 verify=tls_verify(),
-                                headers={"Authorization": "Bearer {}".format(get_auth_token())})
-
-        app.logger.info(response)
+                                headers={'Authorization': 'Bearer {}'.format(get_auth_token())})
+        
     else:
-        requests.post("{}/clients".format(os.environ.get("IDP_ADMIN_REALM_URL")),
+        requests.post('{}/clients'.format(os.environ.get('IDP_ADMIN_REALM_URL')),
                       json=new_or_updated_client,
                       verify=tls_verify(),
-                      headers={"Authorization": "Bearer {}".format(get_auth_token())})
+                      headers={'Authorization': 'Bearer {}'.format(get_auth_token())})
 
     return jsonify(success=True)
 
@@ -92,10 +89,10 @@ def create_or_update(clientId):
 def delete(clientId):
     client = get_client(clientId)
 
-    requests.delete("{}/clients/{}".format(os.environ.get("IDP_ADMIN_REALM_URL"),
-                                           client["id"]),
+    requests.delete('{}/clients/{}'.format(os.environ.get('IDP_ADMIN_REALM_URL'),
+                                           client['id']),
                     verify=tls_verify(),
-                    headers={"Authorization": "Bearer {}".format(get_auth_token())})
+                    headers={'Authorization': 'Bearer {}'.format(get_auth_token())})
 
     return jsonify(success=True)
 
