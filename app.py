f2486e6481bd37761deca2666c32882ef62cd704
Leandro Beretta <lea.beretta@gmail.com>
2020-11-19 00:53:25 -0300

    create/update/delete implemented

--- app.py
+++ app.py
@@ -1,35 +1,103 @@
 from flask import Flask, request, jsonify
-import logging, requests, os
+import logging
+import requests
+import os
 
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 
+def get_auth_token():
+    data = {
+        "grant_type": "password",
+        "username": os.environ.get("IDP_USERNAME"),
+        "password": os.environ.get("IDP_PASSWORD"),
+        "client_id": "admin-cli"
+    }
+
+    response = requests.post("{}/protocol/openid-connect/token".format(
+        os.environ.get("IDP_MASTER_REALM_URL")), verify=tls_verify(), data=data)
+
+    app.logger.info(response.json())
+
+    return response.json()["access_token"]
+
+
+def tls_verify():
+    if os.environ.get("IDP_VERIFY_TLS") == "false":
+        return False
+
+    return True
+
+
+def get_client(clientId):
+    response = requests.get("{}/clients".format(os.environ.get("IDP_ADMIN_REALM_URL")),
+                            verify=tls_verify(), 
+                            headers={"Authorization": "Bearer {}".format(get_auth_token())})
+
+    clients = response.json()
+    app.logger.info(clients)
+    for client in clients:
+        app.logger.info(client)
+        if client["clientId"] == clientId:
+            app.logger.info(client["id"])
+            return client
+
+    return None
+
+
 @app.route('/.well-known/openid-configuration', methods=['GET'])
 def get_openid_configuration():
-    tls_verify = True
-    
-    if os.environ.get("IDP_VERIFY_TLS") == "false":
-        tls_verify = False
-
-    response = requests.get("{}/.well-known/openid-configuration".format(os.environ.get("IDP_REALM_URL")), verify=tls_verify)
+    response = requests.get("{}/.well-known/openid-configuration".format(
+        os.environ.get("IDP_REALM_URL")), verify=tls_verify())
 
     return response.json()
 
 
 @app.route('/clients/<clientId>', methods=['PUT'])
 def create_or_update(clientId):
-    app.logger.info(clientId)
+    data = request.json
 
-    return request.data
+    new_or_updated_client = {
+        "clientId": data["client_id"],
+        "secret": data["client_secret"],
+        "enabled": True,
+        "publicClient": False
+    }
+
+    app.logger.info("new_or_updated_client")
+    app.logger.info(new_or_updated_client)
+
+    client = get_client(clientId)
+
+    if client is not None:
+        response = requests.put("{}/clients/{}".format(os.environ.get("IDP_ADMIN_REALM_URL"),
+                                                       client["id"]),
+                                json=new_or_updated_client,
+                                verify=tls_verify(),
+                                headers={"Authorization": "Bearer {}".format(get_auth_token())})
+
+        app.logger.info(response)
+    else:
+        requests.post("{}/clients".format(os.environ.get("IDP_ADMIN_REALM_URL")),
+                      json=new_or_updated_client,
+                      verify=tls_verify(),
+                      headers={"Authorization": "Bearer {}".format(get_auth_token())})
+
+    return jsonify(success=True)
 
 
 @app.route('/clients/<clientId>', methods=['DELETE'])
 def delete(clientId):
-    app.logger.info(clientId)
+    client = get_client(clientId)
 
-    return None
+    requests.delete("{}/clients/{}".format(os.environ.get("IDP_ADMIN_REALM_URL"),
+                                           client["id"]),
+                    verify=tls_verify(),
+                    headers={"Authorization": "Bearer {}".format(get_auth_token())})
+
+    return jsonify(success=True)
 
 
 if __name__ == '__main__':
