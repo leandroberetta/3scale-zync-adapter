99ca8d1c7e5a368771cc9dca80fb6d45c1f3a4d9
Leandro Beretta <lea.beretta@gmail.com>
2020-11-18 22:08:47 -0300

    testing debug logger

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, jsonify
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
+
 
 @app.route('/.well-known/openid-configuration', methods=['GET'])
 def wellKnown():
     res = {}
+    app.logger.info("test")
 
     res["token_endpoint"] = "http://idp.example.com/auth/realm/token"
 
     return jsonify(res)
+
 
 @app.route('/clients/<clientId>', methods=['PUT'])
 def createOrUpdate(clientId):
@@ -17,11 +23,13 @@ def createOrUpdate(clientId):
 
     return request.data
 
+
 @app.route('/clients/<clientId>', methods=['DELETE'])
 def delete(clientId):
     app.logger.info(clientId)
 
     return None
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)  # pragma: no cover
